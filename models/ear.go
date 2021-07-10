bd6209213ee178df49b92a56297d75c3e10e8e09
gotomsak <gotomsak@gmail.com>
2021-07-11 02:34:37 +0900

    瞬きの閾値を含めたdata形式に変更

--- models/ear.go
+++ models/ear.go
@@ -4,11 +4,14 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // EarData bindしてそのまま保存
 type EarData struct {
-	ID       primitive.ObjectID `json:"id" bson:"_id"`
-	UserID   int                `json:"user_id"`
-	RightEar float64            `json:"right_ear"`
-	LeftEar  float64            `json:"left_ear"`
-	Date     string             `json:"date" bson:"date"`
+	ID     primitive.ObjectID `json:"id" bson:"_id"`
+	UserID int                `json:"user_id"`
+
+	RightEarList []float64 `json:"right_ear_list"`
+	RightEarT    float64   `json:"right_ear_t"`
+	LeftEarList  []float64 `json:"left_ear_list"`
+	LeftEarT     float64   `json:"left_ear_t"`
+	Date         string    `json:"date" bson:"date"`
 	// Environment string             `json:"environment"`
 }
 
